e01683056181880ba317192af17d64cff8689b20
Artemy Cold <avorobev341@gmail.com>
2024-12-05 17:42:44 +0400

    Внедрение shared модуля в главный модуль app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked {
 
-}
+
+    constructor(
+
+    ) {
+
+    };
+
+
+    public ngAfterContentChecked(): void {
+        
+    };
+
+
+    protected getState(outlet: RouterOutlet) {
+
+        return outlet.activatedRouteData['state'];
+    };
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './module/component/nav/nav.component';
+import { SharedNavModule } from './module/component';
 
 @NgModule({
     declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        SharedNavModule
     ],
     providers: [],
     bootstrap: [AppComponent]
