7d9d4175b1ebe38408d844b4423423ccecdff6ca
Artemy Cold <avorobev341@gmail.com>
2024-12-07 13:10:36 +0400

    Заготовка для lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full',
+    },
+    {
+        path: 'main',
+        loadChildren: () => import('').then(m => m.)
+    },
+    {
+        path: 'admission-guide',
+        loadChildren: () => import('').then(m => m.)
+    },
+    {
+        path: 'advantages',
+        loadChildren: () => import('').then(m => m.)
+    },
+    {
+        path: 'contacts',
+        loadChildren: () => import('').then(m => m.)
+    },
+    {
+        path: 'feedback',
+        loadChildren: () => import('').then(m => m.)
+    },
+    {
+        path: 'faq',
+        loadChildren: () => import('').then(m => m.)
+    },
+    {
+        path: '**',
+        redirectTo: 'main',
+        pathMatch: 'full',
+    }];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
